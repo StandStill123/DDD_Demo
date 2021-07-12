fe8898a6e9b41853b99deb8a63d89f1e9ff0ab5d
zhucj_sh <zhucj_sh@ushareit.com>
2021-07-12 14:04:38 +0800

    add

---
+++ demo_domain/src/main/java/com/demo/domain/common/Identifiable.java
@@ -0,0 +1,11 @@
+package com.demo.domain.common;
+
+import java.io.Serializable;
+
+/**
+ * @Author: zhucj_sh
+ * @Date: 2021/7/5 20:13
+ */
+public interface Identifiable extends Serializable {
+    String value();
+}
